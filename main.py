1ce2c30d586fa27f3ca21f2cd1ce88a5df87ad35
icarus <aldenesterhuizen1@gmail.com>
2026-09-15 21:22:41 +0200

    added first set valeus of idea

--- main.py
+++ main.py
@@ -3,3 +3,11 @@
 greetMessage = "---- welcome to truck tracker v0.1 ----"
 
 print(greetMessage)
+
+driverInfo = input("What is the drivers name: ")
+truckIDInfo = input("What is the truck ID: ")
+routeInfo = input("What is the route beign taken (eg. x to y): ")
+minTempInfo = input("What is the minimum temp that the truck may reach: ")
+maxTempInfo = input("What is the max temp that the truck may reach: ")
+startTime = input("What time did the route start(This includes date): ")
+finishTime = input*"What time did the route finish(this includes date): "
