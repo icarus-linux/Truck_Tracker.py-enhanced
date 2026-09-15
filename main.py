638de9fbfb02822b9730aa95617ce5bdf8321908
icarus <aldenesterhuizen1@gmail.com>
2026-09-15 21:23:10 +0200

    fixxed formatting

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ routeInfo = input("What is the route beign taken (eg. x to y): ")
 minTempInfo = input("What is the minimum temp that the truck may reach: ")
 maxTempInfo = input("What is the max temp that the truck may reach: ")
 startTime = input("What time did the route start(This includes date): ")
-finishTime = input*"What time did the route finish(this includes date): "
+finishTime = input("What time did the route finish(this includes date): ")
